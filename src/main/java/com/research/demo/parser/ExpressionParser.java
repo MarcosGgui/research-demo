ee6dfd124f46f36e6bf2ae0bf9610af29d868f8f
Marcos Gui <marcos.gui@spsoft-cn.com>
2019-04-16 10:14:15 +0800

    [ADD] ADD ExpressionParser

--- src/main/java/com/research/demo/parser/ExpressionParser.java
+++ src/main/java/com/research/demo/parser/ExpressionParser.java
@@ -1,7 +1,10 @@
 package com.research.demo.parser;
 
 import com.research.demo.domain.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 import net.objecthunter.exp4j.function.Function;
@@ -36,15 +39,21 @@ public class ExpressionParser{
             .function(new Function("SUM", 1){
                 @Override
                 public double apply(double... args) {
-                    log.debug("Arguments in Function" + args[0]);
-                    //TODO do something to define the function "sum"
                     return 0;
                 }
-            }).function(new Function("Year", 1){
+            }).function(new Function("YEAR", 1){
                 @Override
                 public double apply(double... args) {
-                    log.info(args);
+                    expression = formatFunctionExpression("YEAR");
                     functionValue = "date.year";
+                    return 0;
+                }
+            })
+            .function(new Function("MONTH"){
+                @Override
+                public double apply(double... args) {
+                    expression = formatFunctionExpression("MONTH");
+                    functionValue = "date.monthOfYear";
                     return 0;
                 }
             });
@@ -55,12 +64,33 @@ public class ExpressionParser{
 
         for (Field v : variables) {
             e.setVariable(v.getName(), 0);
+            e.evaluate();
             this.expression = this.expression.replace(v.getName(), "doc['" + v.getName() + "']." + functionValue);
 
         }
-
-        log.info("Calculate Result: " + e.evaluate());
-        log.info("Expression: " + expression);
         return expression;
+    }
+
+    private String formatFunctionExpression(String functionName) {
+        List<Character> output = new ArrayList<>();
+        Stack<Character> stack = new Stack<>();
+        char[] chars = this.expression.toCharArray();
+        for (Character ch : chars) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else if (!stack.empty() && stack.peek() == '(') {
+                if (ch != ')') {
+                    output.add(ch);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (Character ch : output) {
+            builder.append(ch);
+        }
+        return this.expression.replace(functionName + "(" + builder.toString() + ")", builder.toString());
     }
 }
